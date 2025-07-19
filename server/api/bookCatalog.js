1a5f360f8121d55d53c49340a5e02035d93b253e
g1orgi89 <g.ahmeteli@gmail.com>
2025-07-19 19:42:28 +0400

    Fix: Replace incorrect import with correct adminAuth import in bookCatalog.js

--- server/api/bookCatalog.js
+++ server/api/bookCatalog.js
@@ -6,7 +6,7 @@
 
 const express = require('express');
 const BookCatalog = require('../models/BookCatalog');
-const { adminAuth } = require('../middleware/auth');
+const { adminAuth } = require('../middleware/adminAuth');
 const logger = require('../utils/logger');
 
 const router = express.Router();
