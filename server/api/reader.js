715a83de34a3091a49ae3f26c063367409c19ebb
g1orgi89 <g.ahmeteli@gmail.com>
2025-08-30 20:25:42 +0400

    Update reader.js

--- server/api/reader.js
+++ server/api/reader.js
@@ -695,7 +695,7 @@ router.post('/quotes', telegramAuth, async (req, res) => {
 
     // Основная логика добавления цитаты
     try {
-      const result = await quoteHandler.handleQuote(userId, text);
+      const result = await quoteHandler.handleQuote(userId, text, author, sourse);
 
       if (!result.success) {
         return res.status(400).json({
