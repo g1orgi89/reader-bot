dfb403aeefb94b0bddc9d7d38beeb86c1679a4bc
g1orgi89 <g.ahmeteli@gmail.com>
2025-09-07 20:02:13 +0400

    Update rateLimiting.js

--- server/middleware/rateLimiting.js
+++ server/middleware/rateLimiting.js
@@ -52,7 +52,7 @@ function skipOptionsRequests(req) {
  */
 const generalLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute (reduced for testing)
-  max: 5, // Limit each IP to 5 requests per minute (reduced for testing)
+  max: 20, // Limit each IP to 5 requests per minute (reduced for testing)
   message: 'Too many requests from this IP, please try again later',
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
